217ee2dc506e668f5635d2399c7e6c338f1addf4
Anton Machkasov <anton.mackasov@gmail.com>
2013-06-12 16:38:03 +0300

    Added LineManager and GameManager

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	public int score = 0;
+	
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
