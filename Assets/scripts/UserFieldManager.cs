9b173f80c8ef69f391e0f77c6193117f78eb8a70
Anton Machkasov <anton.machkasov@gmail.com>
2013-06-18 15:07:35 +0300

    Cnahged mode of drawing to mesh

--- Assets/scripts/UserFieldManager.cs
+++ Assets/scripts/UserFieldManager.cs
@@ -9,7 +9,6 @@ public class UserFieldManager : MonoBehaviour
 	// 1 - 2nd object to create
 	// -1 - second obj
 	public GameObject brushPrefub;
-	
 	int creationMode = -1;
 	public GameObject[] objectsToCreate;
 	GameObject currentObject = null;
@@ -18,13 +17,17 @@ public class UserFieldManager : MonoBehaviour
 	float minColliderLenght;
 	public Material blockMaterial;
 	PhysicMaterial blockPhysicsMaterial;
-	//LineRenderer lineRenderer;	
+	float blockHeight = 0f;
 	
+	
+	public PhysicMaterial colliderMeshMaterial;
+	//LineRenderer lineRenderer;		
 	public Transform DotPrefab;
 	public GameObject trailPrefab;
 	GameObject ball;
 	GameObject currentTrailRendererObject;
 	Vector3 mousePosition; // Vector3 because of using a raycast.
+	
 	//Vector3 mouseHit;
 	Vector3 newDotPosition;
 	Vector3 lastDotPosition;
@@ -78,12 +81,156 @@ public class UserFieldManager : MonoBehaviour
 	{
 		if (currentObject != null) {				
 			currentObject.transform.position = MousePoint ();									
-			if (b) 
-			{
+			if (b) {
 				currentObject = null;
 				creationMode = -2;			
 			}				
 		}
+	}
+		
+	void createBoxWithoutFrontSide (Vector3 p1, Vector3 p2)
+	{	
+		
+		//TODO: recalculate border. Add one more side.
+		// 
+		Vector3 topLeftFront = p1;
+		Vector3 topRightFront = p2;
+		Vector3 topLeftBack = p1;
+		Vector3 topRightBack = p2;
+				
+		Vector3 bottomLeftFront;
+		Vector3 bottomRightFront;
+		// TODO: check
+		Vector3 backLeft = p1;
+		Vector3 backRight = p2;
+		
+		
+		
+		topRightFront.z = 0.5f;
+		topLeftFront.z = 0.5f;
+		topLeftBack.z = -0.5f;
+		topRightBack.z = -0.5f;
+		
+		float l = Vector3.Magnitude (p1 - p2);		
+		float b = Mathf.Sqrt (l * l + blockHeight * blockHeight);
+		float x1 = l * blockHeight / b;
+		float y1 = blockHeight * blockHeight / b;		
+		
+		bottomLeftFront = topLeftFront;
+		bottomRightFront = topRightFront;
+		
+		/*	
+		bottomLeftFront.y -= y1;
+		bottomLeftFront.x -= x1;
+		bottomRightFront.y -= y1;
+		bottomRightFront.x -= x1;
+		*/
+		
+		bottomLeftFront.y -= blockHeight;
+		bottomRightFront.y -= blockHeight;
+		
+		backLeft = bottomLeftFront;
+		backLeft.z = -0.5f;		
+		backRight = bottomRightFront;
+		backRight.z = -0.5f;
+		
+		
+		GameObject newLedge = new GameObject ();
+		Mesh newMesh = new Mesh ();
+		newLedge.AddComponent<MeshFilter> ();
+		newLedge.AddComponent<MeshRenderer> ();
+		
+		newMesh.vertices = new Vector3[] {topLeftFront, topRightFront, topLeftBack, topRightBack,
+			bottomLeftFront, bottomRightFront, backLeft, backRight};
+		
+		Vector2[] uvs = new Vector2[newMesh.vertices.Length];
+		for (int i = 0; i < uvs.Length; i++) {
+			uvs [i] = new Vector2 (newMesh.vertices [i].x, newMesh.vertices [i].z);
+		}		
+		
+		newMesh.uv = uvs;		
+		newMesh.triangles = new int[] {5,4,6, 6,7,5};
+		newMesh.RecalculateNormals ();		
+		
+		newLedge.GetComponent<MeshFilter> ().mesh = newMesh;
+		addMaterial(newLedge);		
+	}
+	
+	void createBox (Vector3 p1, Vector3 p2)
+	{	
+		
+		//TODO: recalculate border. Add one more side.
+		// 
+		Vector3 topLeftFront = p1;
+		Vector3 topRightFront = p2;
+		Vector3 topLeftBack = p1;
+		Vector3 topRightBack = p2;
+				
+		Vector3 bottomLeftFront;
+		Vector3 bottomRightFront;
+		// TODO: check
+		Vector3 backLeft = p1;
+		Vector3 backRight = p2;
+		
+		
+		
+		topRightFront.z = 0.5f;
+		topLeftFront.z = 0.5f;
+		topLeftBack.z = -0.5f;
+		topRightBack.z = -0.5f;
+		
+		float l = Vector3.Magnitude (p1 - p2);		
+		float b = Mathf.Sqrt (l * l + blockHeight * blockHeight);
+		float x1 = l * blockHeight / b;
+		float y1 = blockHeight * blockHeight / b;		
+		
+		bottomLeftFront = topLeftFront;
+		bottomRightFront = topRightFront;
+		
+		/*	
+		bottomLeftFront.y -= y1;
+		bottomLeftFront.x -= x1;
+		bottomRightFront.y -= y1;
+		bottomRightFront.x -= x1;
+		*/
+		bottomLeftFront.y -= blockHeight;
+		bottomRightFront.y -= blockHeight;
+		
+		backLeft = bottomLeftFront;
+		backLeft.z = -0.5f;		
+		backRight = bottomRightFront;
+		backRight.z = -0.5f;
+		
+		
+		GameObject newLedge = new GameObject ();
+		Mesh newMesh = new Mesh ();
+		newLedge.AddComponent<MeshFilter> ();
+		newLedge.AddComponent<MeshRenderer> ();
+		
+		newMesh.vertices = new Vector3[] {topLeftFront, topRightFront, topLeftBack, topRightBack,
+			bottomLeftFront, bottomRightFront, backLeft, backRight};
+		
+		Vector2[] uvs = new Vector2[newMesh.vertices.Length];
+		for (int i = 0; i < uvs.Length; i++) {
+			uvs [i] = new Vector2 (newMesh.vertices [i].x, newMesh.vertices [i].z);
+		}		
+		
+		newMesh.uv = uvs;		
+		newMesh.triangles = new int[] {5,4,0, 0,1,5, 0,2,3, 3,1,0, 5,4,6, 6,7,5};
+		newMesh.RecalculateNormals ();		
+		
+		newLedge.GetComponent<MeshFilter> ().mesh = newMesh;
+		addMaterial(newLedge);
+	}
+	
+	void  addMaterial(GameObject go)
+	{
+		if (blockMaterial) 
+		go.renderer.material = blockMaterial;
+		go.AddComponent<MeshCollider> ();
+		if (blockPhysicsMaterial) 
+			go.GetComponent<MeshCollider> ().material = blockPhysicsMaterial;	
+		go.GetComponent<MeshCollider> ().material  = colliderMeshMaterial;
 	}
 	
 	void DrawLine ()
@@ -121,7 +268,17 @@ public class UserFieldManager : MonoBehaviour
 						float deltaYPerCollider = (deltaYW - deltaYT) / amountOfColliders;						
 						
 						for (int i = 0; i < amountOfColliders; i++) {
-							CreateBoxCollider (new Vector3 (lastDotPosition.x + deltaXPerCollider, lastDotPosition.y + deltaYPerCollider, 0));
+							//CreateBoxCollider (new Vector3 (lastDotPosition.x + deltaXPerCollider, lastDotPosition.y + deltaYPerCollider, 0));
+							Vector3 nextPoint = new Vector3 (lastDotPosition.x + deltaXPerCollider, lastDotPosition.y + deltaYPerCollider, 0);
+							if (nextPoint.x < lastDotPosition.x) {
+								createBox (lastDotPosition, nextPoint);
+								createBoxWithoutFrontSide (nextPoint, lastDotPosition);
+								lastDotPosition = nextPoint;
+							} else {
+								createBox ( nextPoint, lastDotPosition);
+								createBoxWithoutFrontSide ( lastDotPosition,nextPoint);
+								lastDotPosition = nextPoint;
+							}
 						}
 					}
 						
@@ -165,15 +322,25 @@ public class UserFieldManager : MonoBehaviour
 	}
 	
 	void MouseUp ()
-	{		
+	{	
 		newDotPosition = MousePoint ();
-		CreateBoxCollider (newDotPosition);
+		if (newDotPosition.x > lastDotPosition.x) {
+			createBox (lastDotPosition, newDotPosition);
+			createBoxWithoutFrontSide (lastDotPosition, newDotPosition);
+			lastDotPosition = newDotPosition;
+		} else {
+			createBox (newDotPosition, lastDotPosition);
+			createBoxWithoutFrontSide (newDotPosition, lastDotPosition);
+			lastDotPosition = newDotPosition;
+		}
+		
+		//CreateBoxCollider (newDotPosition);
 		dragging = false;
 		lastPointExist = false;	
-		try
-		{
+		try {
 			currentTrailRendererObject.transform.position = lastDotPosition;
-		} catch(NullReferenceException e){}
+		} catch (NullReferenceException e) {
+		}
 	}
 	
 	void CreateBoxCollider (Vector3 newDotPosition)
@@ -181,7 +348,7 @@ public class UserFieldManager : MonoBehaviour
 		if (lastPointExist && wholeDistance < maxDistance) {
 			//GameObject colliderKeeper = new GameObject ("collider");            
 			//BoxCollider bc = colliderKeeper.AddComponent<BoxCollider> ();	
-			GameObject colliderKeeper = (GameObject) GameObject.Instantiate(brushPrefub);
+			GameObject colliderKeeper = (GameObject)GameObject.Instantiate (brushPrefub);
 			//SphereCollider bc = colliderKeeper.GetComponent<SphereCollider> ();
 			BoxCollider bc = colliderKeeper.GetComponent<BoxCollider> ();
 				
@@ -202,7 +369,7 @@ public class UserFieldManager : MonoBehaviour
 			lastDotPosition = newDotPosition;
 			lastPointExist = true;
 		}
-	}	
+	}
 	
 	public void ChooseObjectToCreate (int _index)
 	{	
@@ -239,10 +406,10 @@ public class UserFieldManager : MonoBehaviour
 		GUI.TextArea (new Rect (125, 50, 50, 20), x.ToString ());		
 		
 		GUI.BeginGroup (new Rect (Screen.width / 2 - 100, 2, 200, 20));
-			GUI.Box (new Rect (0, 0, 200, 20), statusBarBorder);
-			GUI.BeginGroup (new Rect (2, 0, (int)x * 2, 16));
-				GUI.DrawTexture (new Rect (0, 0, 200, 20), markerTexture, ScaleMode.ScaleAndCrop);
-			GUI.EndGroup ();
+		GUI.Box (new Rect (0, 0, 200, 20), statusBarBorder);
+		GUI.BeginGroup (new Rect (2, 0, (int)x * 2, 16));
+		GUI.DrawTexture (new Rect (0, 0, 200, 20), markerTexture, ScaleMode.ScaleAndCrop);
+		GUI.EndGroup ();
 		GUI.EndGroup ();
 		
 		
